e83614808ac787b090305519a33bb8ebadc03e03
Aleksandr Razumov <ernado@ya.ru>
2021-12-01 13:46:17 +0300

    feat: add web

--- gen.go
+++ gen.go
@@ -1,3 +1,3 @@
 package tgstatus
 
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema .oas/openapi.yaml --target internal/oas -package oas --clean
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen --schema _web/openapi.yaml --target internal/oas -package oas --clean
